069a24c32ac796e9add773e090dc9e758d8b1c0b
DalekBaldwin <DalekBaldwin@users.noreply.github.com>
2015-07-15 20:48:12 -0700

    explain stuff

--- src/super-scratch.lisp
+++ src/super-scratch.lisp
@@ -1,8 +1,10 @@
 (in-package :super-scratch)
 (in-readtable :super-scratch)
 
-#!
+#! ;; don't actually load your scratch code when loading super-scratch
 (progn
+  ;; C-x C-e will read a form starting from the opening paren,
+  ;; so wrap your top-level package binding in a progn
   #@some-package
   (your code to evaluate in :some-package goes here
         #@some-other-package
