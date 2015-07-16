b90dbb960880cecb79f9ea9922dbc262bb3508b9
DalekBaldwin <DalekBaldwin@users.noreply.github.com>
2015-07-15 20:42:18 -0700

    herp

--- src/super-scratch.lisp
+++ src/super-scratch.lisp
@@ -4,4 +4,6 @@
 #!
 (progn
   #@some-package
-  (your code to evaluate in :some-package goes here))
+  (your code to evaluate in :some-package goes here
+        #@some-other-package
+        (and your sub-expressions to evaluate in :some-other-package go here)))
